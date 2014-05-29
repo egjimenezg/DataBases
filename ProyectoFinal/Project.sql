2d0202ab4d3b21d6303381bc3260db7ea521b3ee
egjimenezg <egjimenezg@gmail.com>
2014-05-29 10:51:27 -0500

    Nuevas tablas a la base

--- ProyectoFinal/Project.sql
+++ ProyectoFinal/Project.sql
@@ -62,6 +62,107 @@ CREATE TABLE AreaLuna(
     FOREIGN KEY(idLuna) REFERENCES Luna(idLuna) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
+CREATE TABLE Gas(
+    idGas INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombreGas VARCHAR(30) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE GasArea(
+    idGas INTEGER NOT NULL,
+    idArea INTEGER NOT NULL,
+    PRIMARY KEY(idGas,idArea),
+    FOREIGN KEY(idGas) REFERENCES Gas(idGas) ON DELETE CASCADE,
+    FOREIGN KEY(idArea) REFERENCES Area(idArea) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE Especie(
+    idEspecie INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombreCientifico VARCHAR(30) NOT NULL,
+    nombreLocal VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Fauna(
+    idEspecie INTEGER NOT NULL,
+    noExtremidades INTEGER NOT NULL,
+    cantidadOjos INTEGER NOT NULL,
+    numeroEjemplares INTEGER NOT NULL,
+    FOREIGN KEY(idEspecie) REFERENCES Especie(idEspecie)
+);
+
+CREATE TABLE Flora(
+    idEspecie INTEGER NOT NULL,
+    FOREIGN KEY(idEspecie) REFERENCES Especie(idEspecie)    
+);
+
+CREATE TABLE Mineral(
+    nombreCientifico VARCHAR(30) NOT NULL,
+    nombreLocal VARCHAR(30) NOT NULL,
+    existencia INTEGER(5) NOT NULL
+);
+
+CREATE TABLE AlimentoEspecie(
+    idConsumidor INTEGER NOT NULL,
+    idEspecie INTEGER NOT NULL,
+    esPrincipal TINYINT(1) NOT NULL,
+    PRIMARY KEY(idConsumidor,idEspecie),
+    FOREIGN KEY(idConsumidor) REFERENCES Especie(idEspecie) ON DELETE CASCADE,
+    FOREIGN KEY(idEspecie) REFERENCES Especie(idEspecie) ON DELETE CASCADE
+);
+
+CREATE TABLE Proyecto(
+    idProyecto INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    proposito VARCHAR(100) NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_final DATE NOT NULL
+);
+
+CREATE TABLE ProyectoFloraFauna(
+    idProyectoFloraFauna INTEGER NOT NULL,
+    aprendizajeDelIdioma DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY(idProyectoFloraFauna) REFERENCES Proyecto(idProyecto)
+);
+
+CREATE TABLE ProyectoFlora(
+    idProyectoFloraFauna INTEGER NOT NULL,
+    efectoCurativo VARCHAR(50) NOT NULL,    
+    veneno VARCHAR(40) NOT NULL,
+    FOREIGN KEY(idProyectoFloraFauna) REFERENCES Proyecto(idProyecto)
+);
+
+CREATE TABLE ProyectoFauna(
+    idProyectoFloraFauna INTEGER NOT NULL,
+    inteligencia DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY(idProyectoFloraFauna) REFERENCES ProyectoFloraFauna(idProyectoFloraFauna)
+);
+
+CREATE TABLE ProyectoMineral(
+    idProyectoMineral INTEGER NOT NULL,
+    valorComercial DECIMAL(5,2) NOT NULL,
+    propiedades VARCHAR(100) NOT NULL,
+    FOREIGN KEY(idProyectoMineral) REFERENCES Proyecto(idProyecto)
+);
+
+CREATE TABLE Personal(
+    idPersonal INTEGER NOT NULL PRIMARY KEY,
+    nombre VARCHAR(80) NOT NULL,
+    categoria VARCHAR(60) NOT NULL,
+    rango INTEGER NOT NULL
+);
+
+CREATE TABLE Investigador(
+    idPersonal INTEGER NOT NULL,
+    idProyectoFloraFauna INTEGER NOT NULL,
+    FOREIGN KEY(idProyectoFloraFauna) REFERENCES ProyectoFloraFauna(idProyectoFloraFauna),
+    FOREIGN KEY(idPersonal) REFERENCES Personal(idPersonal)
+);
+
+CREATE TABLE Geologo(
+    idPersonal INTEGER NOT NULL,
+    idProyectoMineral INTEGER NOT NULL,
+    FOREIGN KEY(idPersonal) REFERENCES ProyectoMineral(idProyectoMineral),
+    FOREIGN KEY(idProyectoMineral) REFERENCES ProyectoMineral(idProyectoMineral)
+);
 
 
 INSERT INTO Galaxia(nombre) VALUES("Motorola"),
